02c0de3dfb7c940bc0b2e242faa8e16c58cda9e0
Juandavidmendi <92585848+Juandavidmendi@users.noreply.github.com>
2023-02-07 15:32:00 -0500

    Modificacion del archivo de autorizacion

--- proyecto_back/auth/main_auth.js
+++ proyecto_back/auth/main_auth.js
@@ -1,15 +1,14 @@
 const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
-    
-    try{
-        const token = req.headers.authorization.split("")[1];
-        const decoded = jwt.verify(token, "__recret__");
-        req.usuario = decoded;
-        next();
-    }catch (error){
-        res.status(401);
-        res.json({code:4, msg:"no tienes autorizacion para ver el contenido"});
+    try {
+        const token = req.headers.authorization.split(" ")[1]
+        const decoded = jwt.verify(token, "__recret__")
+        req.usuario = decoded
+        next()
+    }catch(error){
+        res.status(401)
+        res.json({code:4, msg: "no tiene autoziacion para ver el contenido"})
     }
 }
 
